22894f0ef88de2f1155b9158f16fd926b8473d47
JoeyPsychs <joeypsychs@hotmail.com>
2023-03-30 06:43:43 -0500

    added index.js and an array

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const initialCards = [
+  {
+  Title: "Yosemite Valley",
+  Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+},
+
+{
+  Title: "Lake Louise",
+  Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+},
+
+{
+  Title: "Bald Mountains",
+  Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+},
+
+{
+  Title: "Latemar",
+  Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+},
+
+{
+  Title: "Vanoise National Park",
+  Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+},
+
+{
+  Title: "Lago di Braies",
+  Link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+},
+];
